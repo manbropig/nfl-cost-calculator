da3f9e7369b60144b088177f88cc50bf31ddd9de
Jamie <jamie.tahirkheli@collectivehealth.com>
2018-09-11 23:25:54 -0700

    um working

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,33 @@
-function init() {
-  // collect all of the inputs
-  var inputs = document.querySelector('.NFLCost__input');
-  console.log(inputs);
-}
+(function () {
+  var inputs;
 
-init();
+  function sumAllInputs() {
+    var result = 0;
+    inputs.forEach(function (input) {
+      var number = Number(input.value);
+      result += number;
+    });
+
+    return result;
+  }
+
+  function updateDisplay(value) {
+    var elem = document.querySelector('#result');
+    elem.value = value;
+  };
+
+  function init() {
+    // collect all of the inputs
+    inputs = document.querySelectorAll('.NFLCost__input');
+
+    inputs.forEach(function (input) {
+      input.addEventListener('keyup', function (event) {
+        var sum = sumAllInputs();
+        updateDisplay(sum);
+      });
+    });
+  }
+
+  init();
+
+})();
