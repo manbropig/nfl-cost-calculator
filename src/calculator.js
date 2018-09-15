1e51b4bd1e7066cef41db55bb246978cc75e5bc0
Jamie <jamie.tahirkheli@collectivehealth.com>
2018-09-14 22:02:45 -0700

    Add first event listener

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,32 @@
-function init() {
-  // collect all of the inputs
-  var inputs = document.querySelector('.NFLCost__input');
-  console.log(inputs);
-}
+(function () {
+  /*
+    event listener functions like this one always take an 'event' param
+    the event param has a lot of useful data on it like the 'target' object
+    which has a value (target.value) property that is the value contained
+    in the input
+  */
+  function direcTvChange(event) {
+    // get the value inside the input box
+    var direcTvValue = event.target.value;
+    // assign it to the display input box
+    document.querySelector('#display').value = direcTvValue;
+  }
 
-init();
+  function init() {
+    // grab the direcTv input element as defined in the html with the id='direcTv'
+    var directTvInputElement = document.querySelector('#direcTv');
+    // add an event listener to it so that the browser knows when to act
+    // in this case we pass the `direcTvChange` function (defined above) to the
+    // event listener
+    directTvInputElement.addEventListener('keyup', direcTvChange);
+  }
+
+  init();
+
+})();
+// the above function is called an IIFE (immediately invoked function expression)
+// It's purpose is to encapsulate all data, variables, functions, EVERYTHING so that
+// another js file included in your app later on, does not accidentally call any of the
+// variables/functions/etc... defined inside of the IIFE.
+// You can think of an IIFE as it's own "sandbox" or it's own universe where other universes won't
+// step on it's toes
