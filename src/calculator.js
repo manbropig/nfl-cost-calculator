033f6965c79a8c3f0450fce4c904e485b6857cda
Jamie <jamie.tahirkheli@collectivehealth.com>
2018-10-11 20:03:05 -0700

    Add custom functions to each input

--- src/calculator.js
+++ src/calculator.js
@@ -1,19 +1,38 @@
 (function () {
   var inputs;
+  const direcTVCalc = (value) => Number(value) * 500;
+  const nationalTvCalc = (value) => Number(value) * 60;
+  const barPatronageCalc = (value) => Number(value) * 100;
+  const merchandiseCalc = (value) => Number(value) * 150;
+  const ticketSalesCalc = (value) => Number(value) * 180;
 
-  function sumAllInputs() {
-    var result = 0;
-    inputs.forEach(function (input) {
-      var number = Number(input.value);
-      result += number;
+  // This map uses input.id's as the key and maps to calc fns and result destinations
+  const calculationsMap = {
+    direcTV: { calcFn: direcTVCalc, targetId: 'direcTVResult' },
+    nationalTv: { calcFn: nationalTvCalc, targetId: 'nationalTvResult' },
+    barPatronage: { calcFn: barPatronageCalc, targetId: 'barPatronageResult' },
+    merchandise: { calcFn: merchandiseCalc, targetId: 'merchandiseResult' },
+    ticketSales: { calcFn: ticketSalesCalc, targetId: 'ticketSalesResult' }
+  };
+
+  const calculateAllInputs = () => {
+    var sum = 0;
+    inputs.forEach((input) => {
+      var calculationObj = calculationsMap[input.id];
+      var result = calculationObj.calcFn(input.value);
+      var destination = document.querySelector('#' + calculationObj.targetId);
+      sum += result;
+      // this is a ternary: meaning if the first expression after the "=" is "truthy"
+      // the first value after the "?" is assigned, otherwise the value after the ":"
+      // is assigned
+      destination.innerHTML = result === 0 ? '' : result;
     });
 
-    return result;
-  }
+    return sum;
+  };
 
   function updateDisplay(value) {
-    var elem = document.querySelector('#result');
-    elem.value = value;
+    document.querySelector('#result').innerHTML = value;
   };
 
   function init() {
@@ -22,7 +41,7 @@
 
     inputs.forEach(function (input) {
       input.addEventListener('keyup', function (event) {
-        var sum = sumAllInputs();
+        var sum = calculateAllInputs();
         updateDisplay(sum);
       });
     });
